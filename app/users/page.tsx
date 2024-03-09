07938a42f47c48ae4c8f2712232d03a0fdecc3d6
dangthaii <dangthaiuseful@gmail.com>
2024-03-09 18:54:20 +0700

    Add UsersPage component with user list and background image

---
+++ app/users/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const users: User[] = await fetch(
+    "https://jsonplaceholder.typicode.com/users"
+  ).then((res) => res.json());
+  return (
+    <div>
+      <div className="font-bold">{new Date().toLocaleTimeString()}</div>
+      <h1>Users Page</h1>
+      <Image src="background.jpg" alt="Vercel Logo" width={500} height={500} />
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UsersPage;
